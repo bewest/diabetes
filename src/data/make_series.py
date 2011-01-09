a3f4c8c5b2f22a62f898bb6dd4705dfaab275914
Ben West <bewest@gmail.com>
2011-01-08 18:40:55 -0800

    clean up dead code

--- src/data/make_series.py
+++ src/data/make_series.py
@@ -5,21 +5,8 @@ from matplotlib.backends.backend_cairo import FigureCanvasCairo as FigureCanvas
 
 from matplotlib.figure import Figure
 from matplotlib import pyplot as plt
-import dateutil.parser
 import sys
-import io
-import time, datetime
-import CairoPlot
-import cairo
-import json
-import string
-import itertools
-from contextlib import contextmanager
 from pprint import pformat
-from scikits import timeseries
-import pandas
-import pandas.io
-import pandas.io.parsers
 
 import insulaudit
 import logging
@@ -39,68 +26,6 @@ def get_opt_parser( ):
                     help="don't print status messages to stdout")
 
   return parser
-
-
-class DefaultRecord( object ):
-  __fields__ = [ 'date', 'time', 'reading' ]
-  def __init__( self, header=[] ):
-    self.header = header
-
-  def decode( self, raw ):
-    P       = map( string.strip, raw.split( ) )
-    date    = dateutil.parser.parse( "T".join( P[ 0:2 ] ) )
-    reading = P[ 2 ]
-
-def RecordFactory( object ):
-  def __init__( self, header ):
-    self.header = header
-
-  def decodeFields( self ):
-    if self.header.startswith( '# ' ):
-      if self.header[3:].split( ) == 3:
-        pass
-
-
-class InvalidRecord( Exception ): pass
-
-@contextmanager
-def data_stream( name='-' ):
-  if name == '-':
-    yield sys.stdin
-  stream = open( name, 'r' )
-  try:
-    yield stream
-  except InvalidRecord, e:
-    log.error( 'error %r' % ( e ) )
-    yield stream
-  stream.close( )
-  
-def iter_data( stream ):
-  for x, line in itertools.izip( itertools.count( ),
-                                 stream.readlines( ) ):
-    if line.startswith( '# ' ) is not True:
-      yield line
-  raise StopIteration( stream )
-
-def get_data( stream ):
-  # TODO: sensitivity to timezones!
-  data  = {  }
-  dates = [ ]
-  for datum in iter_data( stream ):
-    P = map( string.strip, datum.split( ) )
-    try:
-      date = dateutil.parser.parse( 'T'.join( 
-                                    P[ 0:2 ] ) ) 
-      dates.append( date )
-      data[ date ] = int( P[ 2 ] )
-      #data[ 'glucose' ].append( P[ 2 ] )
-    except IndexError, e:
-      log.error( 'error %s' % ( e ) )
-
-  return dates, data
-
-def get_pandas( name ):
-  return pandas.io.parsers.parseText( stream )
 
 def get_series( name ):
   from insulaudit.data import glucose
