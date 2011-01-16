6b337aee8831dc78734b29d23610dbdd35b7e188
Ben West <bewest@gmail.com>
2011-01-15 16:34:53 -0800

    maptype-overlay.html seems to be doing the right thing

--- src/data/js/euclid.js
+++ src/data/js/euclid.js
@@ -1,5 +1,4 @@
 
-var FOO = true;
 
 function EuclidProjection(tiles, lng_ticks, lat_ticks ){
   this._num_tiles = tiles;
@@ -7,13 +6,13 @@ function EuclidProjection(tiles, lng_ticks, lat_ticks ){
   this._lat_ticks = lat_ticks;
   // pixels per tile * num tiles
   // XXX: howto get tilesize?
-  var tilesize    = 256 * this._num_tiles;
+  var tilesize    = 256;
   this._grid_size  = new google.maps.Size( tilesize, tilesize );
   this._origin     = new google.maps.Point( this._grid_size.width/2,
                                            this._grid_size.height/2 );
 
-  this._px_per_lng = this._grid_size.width / this._lng_ticks;
-  this._px_per_lat = this._grid_size.height / this._lat_ticks;
+  this._px_per_lng = this._grid_size.width / (this._lng_ticks * this._num_tiles);
+  this._px_per_lat = this._grid_size.height / (this._lat_ticks * this._num_tiles);
   return this;
 }
 
@@ -24,8 +23,9 @@ EuclidProjection.prototype.fromLatLngToPoint = function(ll){
   var x = origin.x + ( this._px_per_lng * ll.lng());
   // Note that latitude is measured from the world coordinate origin
   // at the top left of the map.
-  var y = origin.y + ( this._px_per_lat * ll.lat());
-  //y = y * -1
+  var y = origin.y + ( this._px_per_lat * ll.lat() * -1);
+      // y = y % this._grid_size.height;  
+  //y -= y
 
   //var y = origin.y - this.worldCoordinateLatRange * Math.sin(latRadians);
   //var y = origin.y - this.worldCoordinateLatRange * Math.sin(latRadians);
