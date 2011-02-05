2de55c1189ea1cd9130b08fa04e42aa67c3a4e8d
Ben West <bewest@gmail.com>
2011-02-05 11:29:00 -0800

    now with config

--- src/mock/simple.py
+++ src/mock/simple.py
@@ -4,14 +4,24 @@
 import cherrypy
 from mako.template import Template
 from mako.lookup import TemplateLookup
+
+
+import json
+
 lookup = TemplateLookup(directories=['templates'])
 
 class Root:
     @cherrypy.expose
     def index(self):
-        tmpl = lookup.get_template("index.html")
-        return tmpl.render(salutation="Hello", target="World")
+      tmpl = lookup.get_template("index.html")
+      return tmpl.render(salutation="Hello", target="World")
 
+
+    @cherrypy.expose
+    def config(self):
+      cherrypy.response.headers['content-type'] = 'text/plain'
+      return json.dumps(cherrypy.config, indent=2)
+      
 
 
 if __name__ == '__main__':
