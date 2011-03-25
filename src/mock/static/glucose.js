c532c305d096740ca65405e1b7eeb4cf873a9ace
Ben West <bewest@gmail.com>
2011-03-25 10:30:40 -0700

    Glucose Day scaled correctly on X and Y, however we lost restricted movement in the process.
    --HG--
    branch : projection

--- src/mock/static/glucose.js
+++ src/mock/static/glucose.js
@@ -9,11 +9,43 @@ if (!Proj4js.defs.PLANE) {
  * getZoomForExtent
  * @request Second.js
  * Allows subclasses to independently override the resolution for x or y axis.
+ * map calls:
+ * moveTo
+ * 
+ * explicitly provided for overriding
+ * getExtent
+ * getResolution
+ * getUnits
+ * getScale
+ * getZoomForExtent
+ * getResolutionForZoom
+ * getZoomForResolution
+ * 
+ * Translation
+ * getLonLatFromViewPortPx
+ * getViewPortPxFromLonLat
+ * 
+ * zoomToScale does some scaling
+ *
+ * FixedZoomLevels
+ * missing:
+ * addOptions
+ *
  */
-var GlucoseDay = OpenLayers.Class(OpenLayers.Layer.MapServer, {
+//var GlucoseDay = OpenLayers.Class(OpenLayers.Layer.MapServer, {
+var GlucoseDay = OpenLayers.Class(OpenLayers.Layer.FixedZoomLevels,
+                                  OpenLayers.Layer.MapServer, {
+  RESOLUTIONS: [
+    24/256,
+    400/256
+  ],
   res_x : null,
   res_y : null,
+  initialize: OpenLayers.Layer.MapServer.prototype.initialize,
+  //addOptions: OpenLayers.Layer.MapServer.prototype.initialize,
+  //moveTo: OpenLayers.Map.prototype.moveTo,
   getXResolution: function () {
+    return 24/256;
     var res  = null,
     viewSize = this.map.getSize(),
     extent   = this.getExtent();
@@ -24,6 +56,7 @@ var GlucoseDay = OpenLayers.Class(OpenLayers.Layer.MapServer, {
     return res;
   },
   getYResolution: function () {
+    return 400/256;
     var res  = null,
     viewSize = this.map.getSize(),
     extent   = this.getExtent();
@@ -50,7 +83,7 @@ var GlucoseDay = OpenLayers.Class(OpenLayers.Layer.MapServer, {
 
       extent = this.map.getExtent();
       px     = new OpenLayers.Pixel(
-        (1/res * (lonlat.lon - extent.left)),
+        (1/res_x * (lonlat.lon - extent.left)),
         (1/res_y * (extent.top - (lonlat.lat)) ));
     }
     return px;
@@ -59,11 +92,32 @@ var GlucoseDay = OpenLayers.Class(OpenLayers.Layer.MapServer, {
     var extent = null;
     var size = this.map.getSize();
     var tlPx = new OpenLayers.Pixel(0,0);
+    var bounds = this.calculateBounds();
     //var tlLL = this
-    return this.getMaxExtent();
-
+    return bounds;
   },
-  calculateBounds: function(center, resolution, res_y) {
+  calculateBounds: function(center, res_x, res_y) {
+    var extent = null;
+    var res = this.getResolution(),
+    size, w_deg, h_deg,
+    left, bottom, top, right;
+
+    if (center == null) {
+      center = this.map.getCenter();
+    }
+    res_x = this.getXResolution();
+    res_y = this.getYResolution();
+    if ((center != null) && (res != null)) {
+      size   = this.map.getSize();
+      w_deg  = size.w * res_x;
+      h_deg  = size.h * res_y;
+      bottom = center.lon - w_deg / 2;
+      left   = center.lat - h_deg / 2;
+      top    = center.lon + w_deg / 2;
+      right  = center.lat + h_deg / 2;
+      extent = new OpenLayers.Bounds(bottom, top, left, right);
+    }
+    return extent;
   },
   getLonLatFromViewPortPx: function (viewPortPx) {
     var lonlat = null,
@@ -83,7 +137,7 @@ var GlucoseDay = OpenLayers.Class(OpenLayers.Layer.MapServer, {
         delta_x = viewPortPx.x - (size.w / 2);
         delta_y = viewPortPx.y - (size.h / 2);
 
-        lon = center.lon + delta_x * (res);
+        lon = center.lon + delta_x * (res_x);
         lat = (center.lat - delta_y * (res_y));
         lonlat  = new OpenLayers.LonLat(lon, lat);
 
@@ -94,7 +148,41 @@ var GlucoseDay = OpenLayers.Class(OpenLayers.Layer.MapServer, {
     }
     return lonlat;
   },
+  calculateGridLayout: function(bounds, extent, res) {
+    var res_x = this.getXResolution(),
+        res_y = this.getYResolution();
+
+    var origin  = extent.getCenterLonLat();
+    var tilelon = res_x * this.tileSize.w,
+        tilelat = res_y * this.tileSize.h;
+
+    var offsetlon     = bounds.left - origin.lon,
+        tilecol       = Math.floor(offsetlon/tilelon) - this.buffer,
+        tilecolremain = offsetlon/tilelon - tilecol,
+        tileoffsetx   = -tilecolremain * this.tileSize.w,
+        tileoffsetlon = origin.lon + tilecol * tilelon;
+
+    var offsetlat     = bounds.top - (origin.lat + tilelat),
+        tilerow       = Math.ceil(offsetlat/tilelat) + this.buffer,
+        tilerowremain = tilerow - offsetlat/tilelat,
+        tileoffsety   = -tilerowremain * this.tileSize.h,
+        tileoffsetlat = origin.lat + tilerow * tilelat;
+    var result =  {
+      tilelon: tilelon, tilelat: tilelat,
+      tileoffsetlon: tileoffsetlon, tileoffsetlat: tileoffsetlat,
+      tileoffsetx: tileoffsetx, tileoffsety: tileoffsety
+    };
+    return result;
+  },
   getZoomForExtent: function (extent, closest) {
     return OpenLayers.Layer.prototype.getZoomForExtent.apply(this, arguments);
+  },
+  getURL: function (newParams, altUrl) {
+    var url = OpenLayers.Layer.MapServer.prototype.getURL.apply(this, arguments);
+    return url;
+  },
+  getFullRequestString: function (newParams, altUrl) {
+    var url = OpenLayers.Layer.MapServer.prototype.getFullRequestString.apply(this, arguments);
+    return url;
   }
 });
