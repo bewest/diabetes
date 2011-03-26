fd160381678fa47949ce8ade608de0947d4cb83e
Ben West <bewest@gmail.com>
2011-03-26 11:04:35 -0700

    calculateBounds is used by Map's moveTo.  It's the last piece that performs calculations according to resolution.
    After replacing it, moving, lon lat, and layout all appear
    correct, but restrictedExtent now fails to work as
    expected.
    
    The logic for restrictedExtent is in moveTo, which is a
    massive gnarly function.  This instruments the code so
    that I can inspect that's going on during the
    restrictedExtent logic.  This implementation exactly
    matches the implementation provided by openlayers.org.
    
    --HG--
    branch : projection

--- src/mock/static/glucose.js
+++ src/mock/static/glucose.js
@@ -3,6 +3,8 @@ if (!Proj4js.defs.PLANE) {
   Proj4js.defs['PLANE'] = "+proj=eqc +lat_ts=1 +lon_0=0 +x_0=0 +y_0=0 +a=0 +b=0 +units=ft +k_0=1";
 }
 /**
+ * Hi-jack all resolution calculations to calculate x and y axis
+ * independently.
  *
  * getLonLatFromViewPortPx
  * getViewPortPxFromLonLat
@@ -11,7 +13,7 @@ if (!Proj4js.defs.PLANE) {
  * Allows subclasses to independently override the resolution for x or y axis.
  * map calls:
  * moveTo
- * 
+ *
  * explicitly provided for overriding
  * getExtent
  * getResolution
@@ -20,11 +22,11 @@ if (!Proj4js.defs.PLANE) {
  * getZoomForExtent
  * getResolutionForZoom
  * getZoomForResolution
- * 
+ *
  * Translation
  * getLonLatFromViewPortPx
  * getViewPortPxFromLonLat
- * 
+ *
  * zoomToScale does some scaling
  *
  * FixedZoomLevels
@@ -96,6 +98,7 @@ var GlucoseDay = OpenLayers.Class(OpenLayers.Layer.FixedZoomLevels,
     //var tlLL = this
     return bounds;
   },
+  //
   calculateBounds: function(center, res_x, res_y) {
     var extent = null;
     var res = this.getResolution(),
@@ -133,7 +136,7 @@ var GlucoseDay = OpenLayers.Class(OpenLayers.Layer.FixedZoomLevels,
         res    = this.map.getResolution();
         res_x  = this.getXResolution();
         res_y  = this.getYResolution();
-        
+
         delta_x = viewPortPx.x - (size.w / 2);
         delta_y = viewPortPx.y - (size.h / 2);
 
@@ -148,6 +151,29 @@ var GlucoseDay = OpenLayers.Class(OpenLayers.Layer.FixedZoomLevels,
     }
     return lonlat;
   },
+  setMap: function (map) {
+    OpenLayers.Layer.prototype.setMap.apply(this, arguments);
+    this.map.calculateBounds = function(center, resolution) {
+      return this.baseLayer.calculateBounds(center, resolution);
+    };
+    // :-( After replacing calculateBounds, restrictedExtent does not work as
+    // expected.  The top limit seems to work, but the bottom goes to the edge
+    // of the grid, farther than the restricted bottom..
+    var inspector = new InspectMoveTo(this.map.moveTo, this.map);
+    this.map.moveTo = inspector.run;
+    /*
+    var originalMove = this.map.moveTo;
+    this.map.moveTo = function (lonlat, zoom, options) {
+      var result = originalMove.apply(this, arguments);
+      console.log( 'moveTo', arguments, result );
+      return result;
+    };
+    */
+  },
+  /*
+   * Very odd.  Grid's accepts (bounds, origin, res) but we're receiving
+   * (bounds, extent, res) instead.
+   */
   calculateGridLayout: function(bounds, extent, res) {
     var res_x = this.getXResolution(),
         res_y = this.getYResolution();
@@ -186,3 +212,198 @@ var GlucoseDay = OpenLayers.Class(OpenLayers.Layer.FixedZoomLevels,
     return url;
   }
 });
+
+
+var InspectMoveTo = OpenLayers.Class({
+  context : null,
+  original: null,
+  initialize: function (original, context) {
+    this.original = original;
+    this.context  = context;
+
+    var self = this;
+    this.run = function () {
+      console.log( 'run' );
+      var result = self.original.apply(this, arguments);
+      // var result = self.originalCopy.apply(this, arguments);
+      console.log( arguments, result );
+      return result;
+    };
+    console.log( 'inspector setup done' );
+  },
+  /** ORIGINAL COPY
+   * Method: moveTo
+   *
+   * Parameters:
+   * lonlat - {<OpenLayers.LonLat>}
+   * zoom - {Integer}
+   * options - {Object}
+   */
+  originalCopy: function(lonlat, zoom, options) {
+    if (!options) {
+        options = {};
+    }
+    if (zoom != null) {
+        zoom = parseFloat(zoom);
+        if (!this.fractionalZoom) {
+            zoom = Math.round(zoom);
+        }
+    }
+    // dragging is false by default
+    var dragging = options.dragging;
+    // forceZoomChange is false by default
+    var forceZoomChange = options.forceZoomChange;
+    // noEvent is false by default
+    var noEvent = options.noEvent;
+
+    if (this.panTween && options.caller == "setCenter") {
+        this.panTween.stop();
+    }
+
+    if (!this.center && !this.isValidLonLat(lonlat)) {
+        lonlat = this.maxExtent.getCenterLonLat();
+    }
+
+    if(this.restrictedExtent != null) {
+        // In 3.0, decide if we want to change interpretation of maxExtent.
+        if(lonlat == null) {
+            lonlat = this.getCenter();
+        }
+        if(zoom == null) {
+            zoom = this.getZoom();
+        }
+        var resolution = this.getResolutionForZoom(zoom);
+        var extent = this.calculateBounds(lonlat, resolution);
+        if(!this.restrictedExtent.containsBounds(extent)) {
+            var maxCenter = this.restrictedExtent.getCenterLonLat();
+            if(extent.getWidth() > this.restrictedExtent.getWidth()) {
+                lonlat = new OpenLayers.LonLat(maxCenter.lon, lonlat.lat);
+            } else if(extent.left < this.restrictedExtent.left) {
+                lonlat = lonlat.add(this.restrictedExtent.left -
+                                    extent.left, 0);
+            } else if(extent.right > this.restrictedExtent.right) {
+                lonlat = lonlat.add(this.restrictedExtent.right -
+                                    extent.right, 0);
+            }
+            if(extent.getHeight() > this.restrictedExtent.getHeight()) {
+                lonlat = new OpenLayers.LonLat(lonlat.lon, maxCenter.lat);
+            } else if(extent.bottom < this.restrictedExtent.bottom) {
+                lonlat = lonlat.add(0, this.restrictedExtent.bottom -
+                                    extent.bottom);
+            }
+            else if(extent.top > this.restrictedExtent.top) {
+                lonlat = lonlat.add(0, this.restrictedExtent.top -
+                                    extent.top);
+            }
+        }
+    }
+
+    var zoomChanged = forceZoomChange || (
+                        (this.isValidZoomLevel(zoom)) &&
+                        (zoom != this.getZoom()) );
+
+    var centerChanged = (this.isValidLonLat(lonlat)) &&
+                        (!lonlat.equals(this.center));
+
+
+    // if neither center nor zoom will change, no need to do anything
+    if (zoomChanged || centerChanged || !dragging) {
+
+        if (!this.dragging && !noEvent) {
+            this.events.triggerEvent("movestart");
+        }
+
+        if (centerChanged) {
+            if ((!zoomChanged) && (this.center)) {
+                // if zoom hasnt changed, just slide layerContainer
+                //  (must be done before setting this.center to new value)
+                this.centerLayerContainer(lonlat);
+            }
+            this.center = lonlat.clone();
+        }
+
+        // (re)set the layerContainerDiv's location
+        if ((zoomChanged) || (this.layerContainerOrigin == null)) {
+            this.layerContainerOrigin = this.center.clone();
+            this.layerContainerDiv.style.left = "0px";
+            this.layerContainerDiv.style.top  = "0px";
+        }
+
+        if (zoomChanged) {
+            this.zoom = zoom;
+            this.resolution = this.getResolutionForZoom(zoom);
+            // zoom level has changed, increment viewRequestID.
+            this.viewRequestID++;
+        }
+
+        var bounds = this.getExtent();
+
+        //send the move call to the baselayer and all the overlays
+
+        if(this.baseLayer.visibility) {
+            this.baseLayer.moveTo(bounds, zoomChanged, dragging);
+            if(dragging) {
+                this.baseLayer.events.triggerEvent("move");
+            } else {
+                this.baseLayer.events.triggerEvent("moveend",
+                    {"zoomChanged": zoomChanged}
+                );
+            }
+        }
+
+        bounds = this.baseLayer.getExtent();
+
+        for (var i=0, len=this.layers.length; i<len; i++) {
+            var layer = this.layers[i];
+            if (layer !== this.baseLayer && !layer.isBaseLayer) {
+                var inRange = layer.calculateInRange();
+                if (layer.inRange != inRange) {
+                    // the inRange property has changed. If the layer is
+                    // no longer in range, we turn it off right away. If
+                    // the layer is no longer out of range, the moveTo
+                    // call below will turn on the layer.
+                    layer.inRange = inRange;
+                    if (!inRange) {
+                        layer.display(false);
+                    }
+                    this.events.triggerEvent("changelayer", {
+                        layer: layer, property: "visibility"
+                    });
+                }
+                if (inRange && layer.visibility) {
+                    layer.moveTo(bounds, zoomChanged, dragging);
+                    if(dragging) {
+                        layer.events.triggerEvent("move");
+                    } else {
+                        layer.events.triggerEvent("moveend",
+                            {"zoomChanged": zoomChanged}
+                        );
+                    }
+                }
+            }
+        }
+
+        if (zoomChanged) {
+            //redraw popups
+            for (var i=0, len=this.popups.length; i<len; i++) {
+                this.popups[i].updatePosition();
+            }
+        }
+
+        this.events.triggerEvent("move");
+
+        if (zoomChanged) { this.events.triggerEvent("zoomend"); }
+    }
+
+    // even if nothing was done, we want to notify of this
+    if (!dragging && !noEvent) {
+        this.events.triggerEvent("moveend");
+    }
+
+    // Store the map dragging state for later use
+    this.dragging = !!dragging;
+
+  }
+
+});
+
