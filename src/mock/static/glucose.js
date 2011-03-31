4d4b4e4da5ce36f325467a960c8f6c7812066f2c
Ben West <bewest@gmail.com>
2011-03-30 22:34:15 -0700

    Lost titles in both, and still missing text in glucose, but at least the marker is showing up.
    --HG--
    branch : feature

--- src/mock/static/glucose.js
+++ src/mock/static/glucose.js
@@ -191,6 +191,13 @@ var CartesianLayer = OpenLayers.Class( OpenLayers.Layer.FixedZoomLevels, {
     this.map.calculateBounds = function(center, resolution) {
       return this.baseLayer.calculateBounds(center, resolution);
     };
+    var self = this;
+    this.map.getXResolution = function () {
+      return self.getXResolution.apply(self, arguments);
+    }
+    this.map.getYResolution = function () {
+      return self.getYResolution.apply(self, arguments);
+    }
   },
   /*
    */
@@ -236,6 +243,209 @@ var CartesianLayer = OpenLayers.Class( OpenLayers.Layer.FixedZoomLevels, {
     return url;
   }
   */
+});
+
+// Monkey patch Renderer
+
+OpenLayers.Util.extend(OpenLayers.Renderer.prototype, {
+  drawFeature: function(feature, style) {
+    var bounds = feature.geometry ? feature.geometry.getBounds() : null,
+        rendered = null,
+        location,
+        xOffset, yOffset,
+        res,
+        res_x, res_y;
+    console.log( 'draw Feature' );
+    if (style == null) {
+      style = feature.style;
+    }
+    if (bounds) {
+      if (!bounds.intersectsBounds(this.extent)) {
+        style = {display: 'none'};
+      }
+      rendered = this.drawGeometry(feature.geometry, style, feature.id);
+      if (style.display != 'none' && style.label && rendered !== false) {
+        location = feature.geometry.getCentroid();
+        if (style.labelXOffset || style.labelYOffset) {
+          xOffset = isNaN(style.labelXOffset) ? 0 : style.labelXOffset;
+          yOffset = isNaN(style.labelYOffset) ? 0 : style.labelYOffset;
+          res = this.getResolution();
+          res_x = this.map.getXResolution();
+          res_y = this.map.getYResolution();
+          location.move(xOffset * res_x, yOffset * res_y);
+        }
+        this.drawText(feature.id, style, location);
+        console.log( 'drew text');
+      } else {
+        this.removeText(feature.id);
+      }
+      return rendered;
+    }
+  }
+
+});
+
+// SVG
+
+OpenLayers.Util.extend(OpenLayers.Renderer.SVG.prototype, {
+  setExtent: function(extent, resolutionChanged) {
+    OpenLayers.Renderer.Elements.prototype.setExtent.apply(this, arguments);
+
+    var resolution = this.getResolution(),
+        res_x = this.map.getXResolution(),
+        res_y = this.map.getYResolution(),
+        left  = -extent.left / res_x,
+        top   = extent.top / res_y,
+        extentString, inRange;
+
+    if (resolutionChanged) {
+      this.left = left;
+      this.top = top;
+      extentString = "0 0 " + this.size.w + " " + this.size.h;
+      this.rendererRoot.setAttributeNS(null, 'viewBox', extentString);
+      this.translate(0, 0);
+      return true;
+    } else {
+      inRange = this.translate(left - this.left, top - this.top);
+      if (!inRange) {
+        this.setExtent(extent, true);
+      }
+      return inRange;
+    }
+  },
+  drawCircle: function(node, geometry, radius) {
+    var resolution = this.getResolution(),
+        res_x = this.map.getXResolution(),
+        res_y = this.map.getYResolution(),
+        x     = (geometry.x / res_x + this.left),
+        y     = (this.top - geometry.y / res_y);
+
+    if (this.inValidRange(x, y)) {
+      node.setAttributeNS(null, "cx", x);
+      node.setAttributeNS(null, "cy", y);
+      node.setAttributeNS(null, "r", radius);
+      return node;
+    } else {
+      return false;
+    }
+  },
+  drawRectangle: function(node, geometry) {
+    var resolution = this.getResolution(),
+        res_x  = this.map.getXResolution(),
+        res_y  = this.map.getYResolution(),
+        width  = geometry.width  / res_x,
+        height = geometry.height / res_y,
+        x      = (geometry.x / res_x + this.left),
+        y      = (this.top - geometry.y / res_y);
+
+    if (this.inValidRange(x, y)) {
+      node.setAttributeNS(null, "x", x);
+      node.setAttributeNS(null, "y", y);
+      node.setAttributeNS(null, "width", width);
+      node.setAttributeNS(null, "height", height);
+      return node;
+    } else {
+      return false;
+    }
+  },
+  drawText: function(featureId, style, location) {
+    var resolution = this.getResolution(),
+        res_x  = this.map.getXResolution(),
+        res_y  = this.map.getYResolution(),
+        x      = (location.x / res_x + this.left),
+        y      = (location.y / res_y - this.top),
+        label  = this.nodeFactory(featureId + this.LABEL_ID_SUFFIX, "text"),
+        tspan  = this.nodeFactory(featureId + this.LABEL_ID_SUFFIX, "_tspan", "text"),
+        align  = style.labelAlign || "cm";
+        ;
+
+    label.setAttributeNS(null, "x",  x);
+    label.setAttributeNS(null, "y", -y);
+
+    if (style.fontColor) {
+      label.setAttributeNS(null, "fill", style.fontColor);
+    }
+    if (style.fontOpacity) {
+      label.setAttributeNS(null, "opacity", style.fontOpacity);
+    }
+    if (style.fontFamily) {
+      label.setAttributeNS(null, "font-family", style.fontFamily);
+    }
+    if (style.fontSize) {
+      label.setAttributeNS(null, "font-size", style.fontSize);
+    }
+    if (style.fontWeight) {
+      label.setAttributeNS(null, "font-weight", style.fontWeight);
+    }
+    if (style.labelSelect == true) {
+      label.setAttributeNS(null, "pointer-events", "visible");
+      label._featureId = featureId;
+      tspan._featureId = featureId;
+      tspan._geometry = location;
+      tspan._geometryClass = location.CLASS_NAME;
+    } else {
+      label.setAttributeNS(null, "pointer-events", "none");
+    }
+    label.setAttributeNS(null, "text-anchor",
+        OpenLayers.Renderer.SVG.LABEL_ALIGN[align[0]] || "middle");
+    if (OpenLayers.IS_GECKO == true) {
+      label.setAttributeNS(null, "dominant-baseline",
+          OpenLayers.Renderer.SVG.LABEL_ALIGN[align[1]] || "central");
+
+    } else {
+      label.setAttributeNS(null, "baseline-shift",
+          OpenLayers.Renderer.SVG.LABEL_ALIGN[align[1]] || "-35%");
+    }
+    tspan.textContent = style.label;
+    
+    if (!label.parentNode) {
+      label.appendChild(tspan);
+      this.textRoot.appendChild(label);
+      console.log('added', label, this.textRoot);
+    }
+  
+  },
+
+  clipLine: function(badComponent, goodComponent) {
+    if (goodComponent.equals(badComponent)) {
+      return "";
+    }
+    var resolution = this.getResolution(),
+        res_x = this.map.getXResolution(),
+        res_y = this.map.getYResolution(),
+        maxX  = this.MAX_PIXEL - this.translationParameters.x,
+        maxY  = this.MAX_PIXEL - this.translationParameters.y,
+        x1    = goodComponent.x / res_x + this.left,
+        y1    = this.top - goodComponent.y / res_y,
+        x2    = badComponent.x / res_x + this.left,
+        y2    = this.top - badComponent.y / res_y,
+        k;
+    if (x2 < -maxX || x2 > maxX) {
+      k = (y2 - y1) / (x2 - x1);
+      x2 = x2 < 0 ? -maxX : maxX;
+      y2 = y1 + (x2 - x1) * k;
+    }
+    if (y2 < -maxY || y2 > maxY) {
+      k = (x2 - x1) / (y2 - y1);
+      y2 = y2 < 0 ? -maxY : maxY;
+      x2 = x1 + (y2 - y1) * k;
+    }
+    return x2 + "," + y2;
+  },
+  getShortString: function(point) {
+    var resolution = this.getResolution(),
+        res_x  = this.map.getXResolution(),
+        res_y  = this.map.getYResolution(),
+        x      = (location.x / res_x + this.left),
+        y      = (location.y / res_y - this.top);
+    if (this.inValidRange(x, y)) {
+      return x + "," + y;
+    } else {
+      return false;
+    }
+    
+  }
+
 });
 
 var GlucoseDay = OpenLayers.Class(OpenLayers.Layer.MapServer, CartesianLayer, {
